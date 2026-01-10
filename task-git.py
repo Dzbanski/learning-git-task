32541242c84f08e2153f38d20546bdc49bd61020
Jacek Dzbański <jacek17123@gmail.com>
2026-01-10 23:39:40 +0100

    Add loop to iterate shopping_list and print information about in which shop i was and what i bought

--- task-git.py
+++ task-git.py
@@ -2,3 +2,9 @@ shopping_list = {
     "piekarnia" : ["chleb", "pączek", "bułki"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+
+print("Lista zakupów")
+
+for sklep in shopping_list:
+    print("Idę do", sklep, "i kupuję tam:", shopping_list[sklep] , ".")
+    
