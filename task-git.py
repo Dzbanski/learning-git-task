e52b55ab02172cbc7fa0706486f8ba1bd7926bc6
Jacek Dzbański <jacek17123@gmail.com>
2026-01-10 23:48:34 +0100

    Add string.title() to dict keys and values and also. Add to loop a code that counts bought products

--- task-git.py
+++ task-git.py
@@ -2,9 +2,11 @@ shopping_list = {
     "piekarnia" : ["chleb", "pączek", "bułki"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
-
+ilosc = 0
 print("Lista zakupów")
 
 for sklep in shopping_list:
-    print("Idę do", sklep, "i kupuję tam:", shopping_list[sklep] , ".")
-    
+    print("Idę do", sklep.title(), "i kupuję tam:",", ".join(element.title() for element in shopping_list[sklep]) + ".")
+    ilosc += len(shopping_list[sklep])
+
+print("W sumie kupuję", ilosc, "produktów.")
